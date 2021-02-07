11e544eeeed5a833dc35a36dfc8ea7fb46e538c9
kealanocallaghan2000 <kealanocallaghan@gmail.com>
2021-02-07 14:53:56 +0000

    Looked at basics.

---
+++ basics.py
@@ -0,0 +1,13 @@
+# Kealan OCallaghan 
+# Python Basics
+
+print("Helo, world!")
+
+a = 1
+b = 2.0 # float
+s = "Hello world"
+t = 'Hello single bracket'
+
+print(a, b ,s, t)
+
+print (t[2:6:2]) # prints from 2 - 6 in 2 letter intervals
